322e437c1e53d13d49fc1b397a7c4fc9dd0b2723
Greg <the.groovy.gregory+gitkraken@gmail.com>
2017-10-08 18:01:37 +0200

    FIX: removing old rx4-related code

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,7 @@
 import CompositeSubscription from './composite-subscription';
 import createSubject from './create-subject';
-import toESObservable from './rx4/to-es-observable';
-
-const rx4 = {
-  toESObservable
-};
 
 export {
   CompositeSubscription,
-  createSubject,
-  rx4
+  createSubject
 };
